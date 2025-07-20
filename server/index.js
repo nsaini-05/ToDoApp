6f7de3259c151163f7b3b8f09a8e0dc63f1d9a0c
Neeraj Saini <asaini7788@gmail.com>
2025-07-20 12:56:00 -0600

    Host from static

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
-
+import path from "path";
 import { dbConnection } from "./database.js";
 import todoRoutes from "./routes/todoRoutes.js";
 import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(cors());
@@ -23,6 +27,14 @@ connectionResult
   });
 
 app.use("/todos", todoRoutes);
+
+const frontendPath = path.resolve(__dirname, "../client/build");
+
+app.use(express.static(frontendPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(frontendPath, "index.html"));
+});
 
 app.get("/", (req, res) => {
   res.send("Hello to ToDO API");
